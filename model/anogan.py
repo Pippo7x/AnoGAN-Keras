6d43f122e2122b099f2ebb81f720341920cd0c4c
Pippo7x <filippo.ferrari19@gmail.com>
2022-08-13 12:51:50 +0200

    fixed gan train and added anogan model

---
+++ model/anogan.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.keras import Sequential, Model
+from tensorflow.keras.layers import Input, Dense
+from .dcgan import DCGAN
+
+class AnoGAN(object):
+    def __init__(self, dcgan: DCGAN):
+        self.dcgan = dcgan
+        self.model = self.anomaly_detector_model(dcgan.generator, dcgan.discriminator)
+        
+    #Anomaly score: A(x) = (1 - lambda) * R(x) + lambda * D(x)
+    def sum_of_residual(self, y_true, y_pred):
+        return tf.reduce_sum(tf.abs(y_true - y_pred))
+
+    def extract_feature_layers(self, model):
+        feature_layers = Sequential(name = "Feature_Layers")
+        feature_layers.add(Input(self.dcgan.image_shape, name = "Input"))
+        for i in range(len(model.layers) - 2):
+            layer = model.get_layer(index = i)
+            feature_layers.add(layer)
+
+        return feature_layers
+
+
+    def anomaly_detector_model(self, generator, discriminator):
+        feature_layers = self.extract_feature_layers(discriminator)
+        feature_layers.trainable = False
+
+        generator_copy = tf.keras.models.clone_model(generator)
+        generator_copy.set_weights(generator.get_weights())
+        generator_copy.trainable = False
+
+        anogan_input = Input(shape = (self.dcgan.noise_dim,))
+        generator_input = Dense(self.dcgan.noise_dim)(anogan_input)
+
+        generator_output = generator_copy(generator_input)
+        feature_layers_output = feature_layers(generator_output)
+
+        model = Model(inputs = anogan_input, outputs = [generator_output, feature_layers_output])
+        model.compile(loss = self.sum_of_residual, loss_weights= [0.9, 0.1], optimizer='adam')
+
+        return model
+
+    def compute_anomaly_score(self, x):
+        z = tf.random.normal([1, self.dcgan.noise_dim])
+
+        feature_layers = self.extract_feature_layers(self.dcgan.discriminator)
+        feature_layers_output = feature_layers.predict(x)
+
+        loss = self.model.fit(z, [x, feature_layers_output], epochs = 500, verbose = 0)
+        similar_data, _ = self.model.predict(z)
+
+        return loss.history["loss"][-1], similar_data
--- model/dcgan.py
+++ model/dcgan.py
@@ -11,23 +11,25 @@ class DCGAN(object):
         self.noise_dim = 100
         self.checkpoint_path = r"./ckpt"
 
+        ###
+        self.cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        self.generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+        self.discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+        ###
+
         #
         self.discriminator = self.build_discriminator(self.image_shape)
-        self.discriminator.compile(loss = "binary_crossentropy", optimizer = "adam", metrics = ["accuracy"])
 
         self.generator = self.build_generator(self.noise_dim)
 
-        self.gan = self.build_gan()
-        self.gan.compile(loss = "binary_crossentropy", optimizer = "adam")
+        #self.gan = self.build_gan()
         
         #per salvare il modello
-        self.checkpoint = tf.train.Checkpoint(gan = self.gan)
+        self.checkpoint = tf.train.Checkpoint(generator = self.generator, discriminator = self.discriminator)
 
     def build_gan(self):
         z = Input(self.noise_dim)
         generated_image = self.generator(z)
-
-        #self.discriminator.trainable = False
 
         decision = self.discriminator(generated_image)
 
@@ -40,28 +42,25 @@ class DCGAN(object):
         # Input shape (None, 100)
         # Output shape (None, 64, 64, 3)
         #
-
-        
-
         model = Sequential(name = "Generator")
 
-        model.add(Dense(4*4*128, input_dim = noise_dim, activation = "relu"))
+        model.add(Dense(4*4*128, input_dim = noise_dim, activation = "relu", use_bias = False))
         model.add(BatchNormalization())
         model.add(Reshape((4,4,128)))
 
-        model.add(Conv2DTranspose(64, (5, 5), strides = (2, 2), padding = "same", activation = "relu"))
+        model.add(Conv2DTranspose(64, (5, 5), strides = (2, 2), padding = "same", activation = "relu", use_bias = False))
         model.add(BatchNormalization())
 
-        model.add(Conv2DTranspose(32, (5, 5), strides = (2, 2), padding = "same", activation = "relu"))
+        model.add(Conv2DTranspose(32, (5, 5), strides = (2, 2), padding = "same", activation = "relu", use_bias = False))
         model.add(BatchNormalization())
 
-        model.add(Conv2DTranspose(16, (5, 5), strides = (2, 2), padding = "same", activation = "relu"))
+        model.add(Conv2DTranspose(16, (5, 5), strides = (2, 2), padding = "same", activation = "relu", use_bias = False))
         model.add(BatchNormalization())
 
-        model.add(Conv2DTranspose(8, (5, 5), strides = (2, 2), padding = "same", activation = "relu"))
+        model.add(Conv2DTranspose(8, (5, 5), strides = (2, 2), padding = "same", activation = "relu", use_bias = False))
         model.add(BatchNormalization())
 
-        model.add(Conv2D(3, (5, 5), activation = "tanh", padding = "same"))
+        model.add(Conv2D(3, (5, 5), activation = "tanh", padding = "same", use_bias = False))
 
         assert model.output_shape == (None, 64, 64, 3)
 
@@ -74,51 +73,76 @@ class DCGAN(object):
         model.add(Conv2D(64, kernel_size = (5, 5), strides = (2, 2), input_shape = image_shape, padding = "same"))
         model.add(BatchNormalization())
         model.add(LeakyReLU(0.2))
+        model.add(Dropout(0.3))
 
         model.add(Conv2D(128, kernel_size = (5, 5), strides = (2, 2), input_shape = image_shape, padding = "same"))
         model.add(BatchNormalization())
         model.add(LeakyReLU(0.2))
-
-        model.add(Conv2D(128, kernel_size = (5, 5), strides = (2, 2), input_shape = image_shape, padding = "same"))
-        model.add(BatchNormalization())
-        model.add(LeakyReLU(0.2))
+        model.add(Dropout(0.3))
 
         model.add(Flatten())
-
-        model.add(Dense(1, activation = "sigmoid"))
+        model.add(Dense(1))
 
         return model
 
     def predict(self, z):
         return self.gan(z)
 
+    def discriminator_loss(self, real_output, fake_output):
+        real_loss = self.cross_entropy(tf.ones_like(real_output), real_output)
+        fake_loss = self.cross_entropy(tf.zeros_like(fake_output), fake_output)
+        total_loss = real_loss + fake_loss
+        return total_loss
+
+    def generator_loss(self, fake_output):
+        return self.cross_entropy(tf.ones_like(fake_output), fake_output)
+
+    def train_step(self, images):
+        noise = tf.random.normal([len(images), self.noise_dim])
+
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            generated_images = self.generator(noise, training=True)
+
+            real_output = self.discriminator(images, training=True)
+            fake_output = self.discriminator(generated_images, training=True)
+
+            gen_loss = self.generator_loss(fake_output)
+            disc_loss = self.discriminator_loss(real_output, fake_output)
+
+        gradients_of_generator = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
+
+        self.generator_optimizer.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
+        self.discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, self.discriminator.trainable_variables))
+
+        return disc_loss, gen_loss
+
+
     def train(self, dataset, epochs):
+        disc_loss_history = []
+        gen_loss_history = []
         for epoch in range(epochs):
             print("Epoch", epoch + 1, end = "  -  ")
 
             disc_loss = []
             gen_loss = []
-
-            for image_batch, label_batch in dataset:
-                noise = tf.random.normal([len(image_batch), self.noise_dim])
-                ###TRAIN DISCRIMINATOR
-
-                real_images = image_batch
-                fake_images = self.generator(noise, training = False)
-
-                X = tf.concat([real_images, fake_images], 0)
-                y = np.array([1] * len(image_batch) + [0] * len(image_batch))
-
-                disc_loss_ = self.discriminator.train_on_batch(X, y)
-
-                ###TRAIN GENERATOR
-                self.discriminator.trainable = False
-                gen_loss_ = self.gan.train_on_batch(noise, np.array([1] * len(image_batch)))
-                self.discriminator.trainable = True 
-
+            for image_batch, _ in dataset:
+                disc_loss_, gen_loss_ = self.train_step(image_batch)
                 disc_loss.append(disc_loss_)
                 gen_loss.append(gen_loss_)
+        
+            print("Generator Loss: {0:.3f}\tDiscriminator Loss: {1:.3f}".format(np.mean(gen_loss), np.mean(disc_loss)))
+            disc_loss_history.append(np.mean(disc_loss))
+            gen_loss_history.append(np.mean(gen_loss))
+        
+        #Salvo il modello
+        self.save()
+        return disc_loss_history, gen_loss_history
+        
 
-            print(f"Generator Loss: {np.mean(gen_loss)}\tDiscriminator Loss: {np.mean(disc_loss)}")
+    def restore(self):
+        self.checkpoint.restore(tf.train.latest_checkpoint(self.checkpoint_path))
 
-        self.checkpoint.save(file_prefix = self.checkpoint_path + r"\ckpt")
+    def save(self):
+        self.checkpoint.save(file_prefix = self.checkpoint_path + r"/ckpt")
+
